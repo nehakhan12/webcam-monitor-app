56adc4dccc3229adb2d9cd3088a312f545da994d
neha <neha.khan1@ontariotechu.net>
2024-08-21 13:17:33 -0400

    Cleans the image folder after images are uploaded

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import cv2
 import time
 from emailing import send_email
 import glob
+import os
 
 video = cv2.VideoCapture(0)
 time.sleep(1)
@@ -9,6 +10,12 @@ time.sleep(1)
 first_frame = None
 status_list = []
 count = 1
+
+
+def clean_folder():
+    images = glob.glob("images/*.png")
+    for image in images:
+        os.remove(image)
 
 while True:
     status = 0
@@ -46,6 +53,7 @@ while True:
 
     if status_list[0] == 1 and status_list[1] == 0:
         send_email(image_with_object)
+        clean_folder()
 
     cv2.imshow("Video", frame)
 
